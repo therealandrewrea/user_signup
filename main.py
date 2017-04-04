af55570f9fbaae0ad004c7ee0f51dd13a9c19a01
Andy Rea <therealandrewrea@gmail.com>
2017-04-04 12:51:35 -0500

    updated and got error messages to appear

--- main.py
+++ main.py
@@ -19,17 +19,13 @@ import re
 
 #CSS for Red Error Messages - apply inline as class
 
-def write_form(username, email, user_error, pwd_error, pwd_error2, email_error):
+def write_form(username, email, user_error, pwd1_error, pwd2_error, email_error):
     header = "<h1>User Signup</h1>"
     user_req = "<label>   Username</label><input type='text' name='username' value=''>"
     pwd_req = "<label>    Password</label><input type='password' name='password1' value=''>"
     pwd_req2 = "<label>Verify Password</label><input type='password' name='password2' value=''>"
     email_req = "<label>Email(optional)</label><input type='text' name='email' value=''>"
     submit_button = "<input type='submit' name='submit'>"
-    user_error = ""
-    pwd1_error = ""
-    pwd2_error = ""
-    email_error = ""
     form = header + '''<form method="post">''' + user_req + user_error + "<br>" + pwd_req + pwd1_error + "<br>" + pwd_req2 + pwd2_error + "<br>" + email_req + email_error + "<br>" + submit_button + '''</form>'''
     return form
 
@@ -39,12 +35,16 @@ class MainHandler(webapp2.RequestHandler):
         self.response.write(blank)    #within the content line, insert relevant errors (user_req + user_error), (pwd_req + pwd1_error), (pwd_req2 + pwd2_error), (email_req + email_error)
 
     def post(self): #for updating of data from post request
-        have_error = False #initialize to false, this tells us if any errors are detected, if not - success!  If so, pass them along
         username = self.request.get("username")
         password1 = self.request.get("password1")
         password2 = self.request.get("password2")
         email = self.request.get("email")
 
+        have_error = False
+        user_error = ""
+        pwd1_error = ""
+        pwd2_error = ""
+        email_error = ""
         #regular expressions for verifying inputs
         user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
         pass_re = re.compile(r"^.{3,20}$")
@@ -57,30 +57,26 @@ class MainHandler(webapp2.RequestHandler):
         def valid_email(email):
             return not email or email_re.match(email)
 
-        #build dictionary to track params and adjust have_error variable if one exists
-        params = dict(username = username,
-                    email = email)
-
         if not valid_username(username):
-            params['user_error'] = "That's not a valid username."
+            user_error += "That's not a valid username."
             have_error = True
-
         if not valid_email(email):
-            params['email_error'] = "That's not a valid email."
+            email_error += "That's not a valid email."
             have_error = True
-
         if not valid_password(password1):
-            params['pwd_error'] = "That wasn't a valid password."
+            pwd1_error += "That wasn't a valid password."
             have_error = True
-        elif password2 != password1:
-            params['pwd_error2'] = "Your passwords don't match."
+        if password2 != password1:
+            pwd2_error += "Your passwords don't match."
+            have_error = True
 
         content = "<h1>Welcome, {0}!".format(username)
 
-        if have_error:
-            self.redirect("/") #placeholder, displays snarky message when have_error is True
-        else:
-            self.write_form(content)
+        if have_error == True:
+            error_page = write_form(username, email, user_error, pwd1_error, pwd2_error, email_error)
+            self.response.write(error_page) #placeholder, displays snarky message when have_error is True
+        elif have_error == False:
+            self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
